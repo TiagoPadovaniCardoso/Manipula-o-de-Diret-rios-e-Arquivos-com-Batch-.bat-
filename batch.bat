dfdbc25ea95e7ad3750d800f503486bb6a5ff65c
TiagoPadovaniCardoso <tiagopadovani801@gmail.com>
2025-02-13 16:39:10 -0300

    Add files via upload

---
+++ batch.bat
@@ -0,0 +1,22 @@
+
+mkdir padovani  
+pause
+cd padovani
+pause
+echo malibabuli > marcos.txt
+type marcos.txt
+pause
+cd .. 
+pause
+mkdir tralalelo 
+pause
+move padovani\marcos.txt tralalelo\marcos.txt
+pause
+cd tralalelo
+type marcos.txt
+pause
+cd .. 
+rmdir /s /q tralalelo
+rmdir /s /q padovani
+del marcos.txt
+  
